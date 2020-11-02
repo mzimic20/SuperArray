2c7a9a1f60a31d4b0f22991d6177f9790f3abdf7
mzimic20 <mzimic20@stuy.edu>
2020-11-02 17:27:27 -0500

    1d complete

--- SuperArray.java
+++ SuperArray.java
@@ -5,11 +5,27 @@ public class SuperArray {
 
   public SuperArray() {
     size = 10;
-    data = new String[10];
+    data = new String[size];
   }
 
   public int size() {
     return size;
+  }
+
+  public boolean add(String element) {
+    int a = -1;
+    for(int i = 0; i < data.length && a == -1; i++) {
+      if (data[i] == null) a = i;
+    }
+    if (a == -1) return false;
+    else {
+      data[a] = element;
+      return true;
+    }
+  }
+
+  public String get(int index) {
+    return data[index];
   }
 
 }
--- Tester.java
+++ Tester.java
@@ -3,6 +3,12 @@ public class Tester {
   public static void main(String[]args) {
     SuperArray words = new SuperArray();
     System.out.println( "size == 10?: " + (words.size() == 10) );
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++) {
+      System.out.println( words.get(i) );
+    }
   }
 
 }
